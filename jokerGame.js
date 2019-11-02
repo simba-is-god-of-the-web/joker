5ea5c567a98b9088da8eadb2a97877ea5c7e8b68
justinChen <yub612@gmail.com>
2019-11-02 15:16:52 +0800

    I'm a loooooser jizz

---
+++ jokerGame.js
@@ -0,0 +1,22 @@
+const suit = {"spade": "黑桃", "heart": "紅心", "diamond": "方塊", "club": "梅花"}
+const numbers = [];
+for(let i=1;i<=13;i++){
+    numbers.push(i);
+}
+function main(){
+    
+}
+
+class player(){
+
+}
+
+die
+
+判斷
+
+
+player
+    card
+
+module.exports = main;
