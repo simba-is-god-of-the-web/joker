6241323238cdf898cf7425e1d289c8ec3835078b
simba <simba.fs@gmail.com>
2019-11-04 22:26:31 +0800

    make index more beautiful

--- public/script/index.js
+++ public/script/index.js
@@ -1,5 +1,10 @@
 $('#start').click(function(){
-	document.location.href = `/room?un=${$('#username').val()}&uid=${$('#id').val()}`;
+	let un = $('#username');
+	if(un.val()){
+		document.location.href = `/room?un=${$('#username').val()}&uid=${$('#id').val()}`;
+	}else{
+		un.addClass('error');
+	}
 });
 $.get('/api/getNewId').then((e)=>{
 	$('#id').val(e.id);
