8c44491fef87f2f748639ac072b07ef8b4b63098
simba <simba.fs@gmail.com>
2019-11-05 22:20:57 +0800

    add sqlite3

--- routes/room.js
+++ routes/room.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3').verbose();
+var userdb = new sqlite3.Database('./db/user.db', function(e){
+	if(e){
+		console.error(e);
+	}else{
+		userdb.run("CREATE TABLE IF NOT EXISTS  user (username TEXT, uid INT)");
+	}
+});
+
 
 /* GET room page. */
 router.get('/', function(req, res, next) {
-  res.render('room', { username: 'JustinChen', id: 7122});
+	userdb.serialize(function(){
+		userdb.each('SELECT username AS uid, username FROM user', function(e, username){
+			console.log(username);
+		});
+		userdb.run(`INSERT INTO user VALUES ('${req.query.un}', ${req.query.uid})`);
+		res.render('room', { username: req.query.un, id: req.query.uid});
+	});
 });
 
 module.exports = router;
