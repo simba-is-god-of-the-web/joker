399e9972cee3b08193fc6a2be23bee1742bdef1c
simba <simba.fs@gmail.com>
2019-11-06 22:34:24 +0800

    add db.js

---
+++ db.js
@@ -0,0 +1,53 @@
+class DB{
+	constructor(setup){
+		this.db = setup.db || [];
+		this.format = setup.format || {};
+		this.name = setup.name || 'memory';
+		this.index = setup.index || 'id';
+	}
+	insert(data = {}){
+		var flag = true;
+		for(var i in data){
+			if(typeof data[i] !== typeof this.format[i]){
+				flag = false;
+			}
+		}
+		if(flag){
+			this.db.push({...data});
+			return this;
+		}else{
+			return new Error('format error');
+		}
+	}
+	select(n = ''){
+		if(!n)			return new Error('n is required');
+		if(!this.index)	return new Error('please set db.index');
+		return this.db.filter((item) => item[this.index] === n);
+	}
+	remove(n = ''){
+		if(!n)			return new Error('n is required');
+		if(!this.index)	return new Error('please set db.index');
+		this.db = this.db.filter((item) => item[this.index] !== n);
+		return;
+		
+	}
+}
+
+var user = new DB({
+	name: 'user',
+	format: {
+		name: '',
+		id: 0
+	},
+	index: 'name'
+});
+user.insert({name: 'simba', id: 7122});
+user.insert({name: 'simba', id: 12});
+user.insert({name: 'jasper', id: 7122});
+user.insert({name: 'justin', id: 7122});
+user.insert({name: 'raccoon', id: 7122});
+console.table(user.db);
+console.table(user.select('simba'));
+user.remove('simba');
+console.table(user.db);
+
--- routes/room.js
+++ routes/room.js
@@ -1,24 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var sqlite3 = require('sqlite3').verbose();
-var userdb = new sqlite3.Database('./db/user.db', function(e){
-	if(e){
-		console.error(e);
-	}else{
-		userdb.run("CREATE TABLE IF NOT EXISTS  user (username TEXT, uid INT)");
-	}
-});
-
+var log = require('../lib/log.js');
 
 /* GET room page. */
 router.get('/', function(req, res, next) {
-	userdb.serialize(function(){
-		userdb.each('SELECT username AS uid, username FROM user', function(e, username){
-			console.log(username);
-		});
-		userdb.run(`INSERT INTO user VALUES ('${req.query.un}', ${req.query.uid})`);
-		res.render('room', { username: req.query.un, id: req.query.uid});
-	});
+	res.render('room', { username: req.query.un, id: req.query.uid});
 });
 
 module.exports = router;
